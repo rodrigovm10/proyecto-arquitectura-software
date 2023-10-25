af9fe60a26ddd9611d9e96d9b1c7df2d1711bb01
YeseniaMartinez24 <martinezalmayesenia904@gmail.com>
2023-10-24 18:22:10 -0600

    footer

--- src/landing/screens/Landing.jsx
+++ src/landing/screens/Landing.jsx
@@ -1,11 +1,13 @@
-import { NavMedium } from '../../pages/NavMedium'
-import { Header } from '../../components/Header'
+import { NavMedium } from "../../pages/NavMedium";
+import { Header } from "../../components/Header";
+import { Footer } from "../Footer";
 
 export function Landing() {
   return (
     <>
       <Header />
       <NavMedium />
+      <Footer />
     </>
-  )
+  );
 }
