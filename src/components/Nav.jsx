d537cde515cd28070ae324c2df6dbeb0003ae364
rodrigovm10 <rv0611200369@gmail.com>
2023-10-30 09:30:04 -0600

    Corrección de erroes en links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,121 +1,124 @@
-import {
-  Flex,
-  Spacer,
-  Link,
-  Image,
-  ButtonGroup,
-  useColorMode,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  IconButton,
-  useBreakpointValue,
-} from "@chakra-ui/react";
-import logo from "../img/logo.png";
-import { useSession } from "../hooks/useSession";
-import { MoonIcon, SunIcon, HamburgerIcon } from "@chakra-ui/icons";
-import { Link as RouterLink } from "react-router-dom";
+import { Flex, Spacer, Link, Image, ButtonGroup, useColorMode, Menu, MenuButton, MenuList, MenuItem, IconButton, useBreakpointValue } from '@chakra-ui/react'
+import logo from '../img/logo.png'
+import { useSession } from '../hooks/useSession'
+import { MoonIcon, SunIcon, HamburgerIcon } from '@chakra-ui/icons'
+import { Link as RouterLink } from 'react-router-dom'
 
 export function Nav({ nombreDelGrupo }) {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const nombreDelGrupoNew = "" || nombreDelGrupo;
-  const { logOut, dataSession, nombreGrupo } = useSession(nombreDelGrupoNew);
-  const isSmallDevice = useBreakpointValue({ base: true, md: false });
+  const { colorMode, toggleColorMode } = useColorMode()
+  const nombreDelGrupoNew = '' || nombreDelGrupo
+  const { logOut, dataSession, nombreGrupo } = useSession(nombreDelGrupoNew)
+  const isSmallDevice = useBreakpointValue({ base: true, md: false })
 
   return (
     <Flex>
-      <Link href="/">
-        <Image src={logo} width={200} />
+      <Link href='/'>
+        <Image
+          src={logo}
+          width={200}
+        />
       </Link>
       <Spacer />
-      <Flex gap={3} alignItems="center">
+      <Flex
+        gap={3}
+        alignItems='center'>
         {isSmallDevice ? (
           <Menu>
             <MenuButton
-              backgroundColor="#181c24"
+              backgroundColor='#181c24'
               as={IconButton}
-              aria-label="Options"
-              icon={<HamburgerIcon color="white" />}
-              variant="outline"
+              aria-label='Options'
+              icon={<HamburgerIcon color='white' />}
+              variant='outline'
             />
             <MenuList>
-              {dataSession.session && nombreGrupo === "Empresa" && (
+              {dataSession.session && nombreGrupo === 'Empresa' && (
                 <>
-                  <MenuItem as={RouterLink} to="/inicio-empresa">
-                    Inicio
+                  <MenuItem>
+                    <RouterLink to='/inicio-empresa'>Inicio</RouterLink>
                   </MenuItem>
-                  <MenuItem as={RouterLink} to="/vacantes">
-                    Vacantes
+                  <MenuItem>
+                    <Link href='/vacantes'>Vacantes</Link>
                   </MenuItem>
                   <MenuItem onClick={logOut}>Cerrar Sesión</MenuItem>
                 </>
               )}
               {dataSession?.session === false && (
                 <>
-                  <MenuItem href="/">Inicio</MenuItem>
-                  <MenuItem href="/login-bdt">Buscadores de Trabajo</MenuItem>
-                  <MenuItem href="/login-empresa">Empresas</MenuItem>
+                  <MenuItem>
+                    <RouterLink to='/'>Inicio</RouterLink>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link href='/login-bdt'>Buscadores de Trabajo</Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link href='/login-empresa'>Empresas</Link>
+                  </MenuItem>
                 </>
               )}
             </MenuList>
           </Menu>
         ) : (
           <>
-            {dataSession.session && nombreGrupo === "Empresa" && (
+            {dataSession.session && nombreGrupo === 'Empresa' && (
               <ButtonGroup>
-                <RouterLink
-                  color="#fff"
-                  to="/inicio-empresa"
-                  _hover={{ color: "#ea754b" }}
-                >
-                  Inicio
-                </RouterLink>
-                <RouterLink
-                  color="#fff"
-                  to="/vacantes"
-                  _hover={{ color: "#ea754b" }}
-                >
-                  Vacantes
-                </RouterLink>
                 <Link
-                  color="#fff"
+                  color='#fff'
+                  to='/inicio-empresa'
+                  _hover={{ color: '#ea754b' }}>
+                  Inicio
+                </Link>
+                <Link
+                  color='#fff'
+                  to='/vacantes'
+                  _hover={{ color: '#ea754b' }}>
+                  Vacantes
+                </Link>
+                <Link
+                  color='#fff'
                   onClick={logOut}
-                  _hover={{ color: "#ea754b" }}
-                >
+                  _hover={{ color: '#ea754b' }}>
                   Cerrar Sesión
                 </Link>
               </ButtonGroup>
             )}
             {dataSession?.session === false && (
               <ButtonGroup>
-                <Link color="#fff" href="/" _hover={{ color: "#ea754b" }}>
+                <RouterLink
+                  color='#fff'
+                  to='/'
+                  _hover={{ color: '#ea754b' }}>
                   Inicio
-                </Link>
+                </RouterLink>
                 <Link
-                  color="#fff"
-                  href="/login-bdt"
-                  _hover={{ color: "#ea754b" }}
-                >
+                  color='#fff'
+                  href='/login-bdt'
+                  _hover={{ color: '#ea754b' }}>
                   Buscadores de Trabajo
                 </Link>
                 <Link
-                  color="#fff"
-                  href="/login-empresa"
-                  _hover={{ color: "#ea754b" }}
-                >
+                  color='#fff'
+                  href='/login-empresa'
+                  _hover={{ color: '#ea754b' }}>
                   Empresas
                 </Link>
               </ButtonGroup>
             )}
           </>
         )}
-        {colorMode === "light" ? (
-          <MoonIcon cursor="pointer" onClick={toggleColorMode} color="#fff" />
+        {colorMode === 'light' ? (
+          <MoonIcon
+            cursor='pointer'
+            onClick={toggleColorMode}
+            color='#fff'
+          />
         ) : (
-          <SunIcon cursor="pointer" onClick={toggleColorMode} />
+          <SunIcon
+            cursor='pointer'
+            onClick={toggleColorMode}
+          />
         )}
       </Flex>
     </Flex>
-  );
+  )
 }
