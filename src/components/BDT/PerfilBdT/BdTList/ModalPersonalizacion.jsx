4a4ec9e0619d2c587617cd8603b5df5a5e6255aa
rodrigovm10 <rv0611200369@gmail.com>
2023-12-05 20:42:31 -0600

    final

--- src/components/BDT/PerfilBdT/BdTList/ModalPersonalizacion.jsx
+++ src/components/BDT/PerfilBdT/BdTList/ModalPersonalizacion.jsx
@@ -1,158 +1,205 @@
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, Select } from "@chakra-ui/react";
-import {opcionesTipografias,opcionesColores} from '../../../../constants/DecoratorCard'
-import { useState } from "react";
-import { estilosTarjetas } from "../../../../constants/DecoratorCard";
-import { registrarStyle } from "../../../../hooks/RegistroBdt";
-import { updatedStyle } from "../../../../hooks/EditarBdt";
+import {
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalCloseButton,
+	ModalBody,
+	ModalFooter,
+	Button,
+	Select,
+	Text
+} from '@chakra-ui/react'
+import { opcionesTipografias, opcionesColores } from '../../../../constants/DecoratorCard'
+import { useState } from 'react'
+import { estilosTarjetas } from '../../../../constants/DecoratorCard'
+import { registrarStyle } from '../../../../hooks/RegistroBdt'
+import { updatedStyle } from '../../../../hooks/EditarBdt'
 
+export function PersonalizarPerfil({ isOpen, onClose, bde, styles }) {
+	const [situacion, setSituacion] = useState('')
+	const [bdeInfo, setBdeInfo] = useState('')
+	const [hab, setHab] = useState('')
 
-export  function PersonalizarPerfil({ isOpen, onClose, bde, styles }) {
-  const [situacion, setSituacion] = useState('');
-  const [bdeInfo, setBdeInfo] = useState('');
-  const [hab, setHab] = useState('');
+	const handleGuardar = async () => {
+		try {
+			await registrarStyle(situacion, bdeInfo, hab, bde)
+			onClose()
+			// Introduce un pequeño retraso (por ejemplo, 500 milisegundos) antes de recargar
 
-  const handleGuardar = async () => {
-    try {
-    await registrarStyle(situacion, bdeInfo, hab, bde);
-  onClose();
-     // Introduce un pequeño retraso (por ejemplo, 500 milisegundos) antes de recargar
-  
-      await window.location.reload();
+			await window.location.reload()
+		} catch (error) {
+			console.error('Error al guardar el estilo:', error)
+			// Puedes agregar aquí lógica adicional en caso de error, como mostrar un mensaje al usuario.
+		}
+	}
 
-  } catch (error) {
-    console.error("Error al guardar el estilo:", error);
-    // Puedes agregar aquí lógica adicional en caso de error, como mostrar un mensaje al usuario.
-  }
-};
+	return (
+		<Modal
+			isOpen={isOpen}
+			onClose={onClose}
+		>
+			<ModalOverlay />
+			<ModalContent>
+				<ModalHeader>Personalizar Perfil</ModalHeader>
+				<ModalCloseButton />
+				<ModalBody>
+					<Text>Situación actual</Text>
+					<Select
+						placeholder='Estilo para la tarjeta situación actual'
+						value={situacion}
+						onChange={e => setSituacion(e.target.value)}
+						mb='2'
+					>
+						{estilosTarjetas.map(estilo => (
+							<option
+								key={estilo.id}
+								value={estilo.id}
+							>{`${estilo.name}`}</option>
+						))}
+					</Select>
 
+					<Text>Información del perfil</Text>
+					<Select
+						placeholder='Estilo para la tarjeta información del perfil'
+						value={bdeInfo}
+						onChange={e => setBdeInfo(e.target.value)}
+						mb='2'
+					>
+						{estilosTarjetas.map(estilo => (
+							<option
+								key={estilo.id}
+								value={estilo.id}
+							>{`${estilo.name}`}</option>
+						))}
+					</Select>
 
-  return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Personalizar Perfil</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <Select
-            placeholder="Estilo de Tarjeta para Situación"
-            value={situacion}
-            onChange={(e) => setSituacion(e.target.value)}
-            mb='2'
-          >
-            {estilosTarjetas.map((estilo) => (
-              <option key={estilo.id} value={estilo.id}>{`Estilo ${estilo.id}`}</option>
-            ))}
-          </Select>
-          
-          <Select
-            placeholder="Estilo de Tarjeta para BDE Info"
-            value={bdeInfo}
-            onChange={(e) => setBdeInfo(e.target.value)}
-            mb='2'
-          >
-            {estilosTarjetas.map((estilo) => (
-              <option key={estilo.id} value={estilo.id}>{`Estilo ${estilo.id}`}</option>
-            ))}
-          </Select>
-
-          <Select
-            placeholder="Estilo de Tarjeta para HAB"
-            value={hab}
-            onChange={(e) => setHab(e.target.value)}
-            mb='2'
-          >
-            {estilosTarjetas.map((estilo) => (
-              <option key={estilo.id} value={estilo.id}>{`Estilo ${estilo.id}`}</option>
-            ))}
-          </Select>
-        </ModalBody>
-        <ModalFooter>
-          <Button colorScheme="blue" onClick={handleGuardar} mr='2'>
-            Guardar
-          </Button>
-          <Button colorScheme="blue" onClick={onClose}>
-            Cerrar
-          </Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-  );
+					<Text>Idiomas y habilidades</Text>
+					<Select
+						placeholder='Estilo para la tarjeta de idiomas y habilidades'
+						value={hab}
+						onChange={e => setHab(e.target.value)}
+						mb='2'
+					>
+						{estilosTarjetas.map(estilo => (
+							<option
+								key={estilo.id}
+								value={estilo.id}
+							>{`${estilo.name}`}</option>
+						))}
+					</Select>
+				</ModalBody>
+				<ModalFooter>
+					<Button
+						colorScheme='blue'
+						onClick={handleGuardar}
+						mr='2'
+					>
+						Guardar
+					</Button>
+					<Button
+						colorScheme='blue'
+						onClick={onClose}
+					>
+						Cerrar
+					</Button>
+				</ModalFooter>
+			</ModalContent>
+		</Modal>
+	)
 }
 
-
 export const PersonalizarPerfilEdicion = ({ isOpen, onClose, bde, styles }) => {
-  const [situacion, setSituacion] = useState(styles.cardSit);
-  const [bdeInfo, setBdeInfo] = useState(styles.cardInfo);
-  const [hab, setHab] = useState(styles.cardHab);
-  
-  const handleGuardar = async () => {
-    try {
-      await updatedStyle(situacion, bdeInfo, hab, bde, styles);
-      onClose();
-      
-  
-      // Introduce un pequeño retraso (por ejemplo, 500 milisegundos) antes de recargar
-      setTimeout(() => {
-        window.location.reload();
-      }, 1000);
-    } catch (error) {
-      console.error("Error al guardar el estilo:", error);
-      // Puedes agregar aquí lógica adicional en caso de error, como mostrar un mensaje al usuario.
-    }
-  };
-  
-  
+	const [situacion, setSituacion] = useState(styles.cardSit)
+	const [bdeInfo, setBdeInfo] = useState(styles.cardInfo)
+	const [hab, setHab] = useState(styles.cardHab)
 
-  return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Personalizar Perfil</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-        <Select
-            placeholder="Estilo de Tarjeta para Situación"
-            value={situacion}
-            onChange={(e) => setSituacion(e.target.value)}
-            mb='2'
-          >
-            {estilosTarjetas.map((estilo) => (
-              <option key={estilo.id} value={estilo.id}>{`Estilo ${estilo.id}`}</option>
-            ))}
-          </Select>
-          
-          <Select
-            placeholder="Estilo de Tarjeta para BDE Info"
-            value={bdeInfo}
-            onChange={(e) => setBdeInfo(e.target.value)}
-            mb='2'
-          >
-            {estilosTarjetas.map((estilo) => (
-              <option key={estilo.id} value={estilo.id}>{`Estilo ${estilo.id}`}</option>
-            ))}
-          </Select>
+	const handleGuardar = async () => {
+		try {
+			await updatedStyle(situacion, bdeInfo, hab, bde, styles)
+			onClose()
 
-          <Select
-            placeholder="Estilo de Tarjeta para HAB"
-            value={hab}
-            onChange={(e) => setHab(e.target.value)}
-            mb='2'
-          >
-            {estilosTarjetas.map((estilo) => (
-              <option key={estilo.id} value={estilo.id}>{`Estilo ${estilo.id}`}</option>
-            ))}
-          </Select>
-        </ModalBody>
-        <ModalFooter>
-          <Button colorScheme="blue" onClick={handleGuardar} mr='2'>
-            Guardar
-          </Button>
-          <Button colorScheme="blue" onClick={onClose}>
-            Cerrar
-          </Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-  );
-};
+			// Introduce un pequeño retraso (por ejemplo, 500 milisegundos) antes de recargar
+			setTimeout(() => {
+				window.location.reload()
+			}, 1000)
+		} catch (error) {
+			console.error('Error al guardar el estilo:', error)
+			// Puedes agregar aquí lógica adicional en caso de error, como mostrar un mensaje al usuario.
+		}
+	}
 
+	return (
+		<Modal
+			isOpen={isOpen}
+			onClose={onClose}
+		>
+			<ModalOverlay />
+			<ModalContent>
+				<ModalHeader>Personalizar Perfil</ModalHeader>
+				<ModalCloseButton />
+				<ModalBody>
+					<Text>Situación actual</Text>
+					<Select
+						placeholder='Estilo para la tarjeta situación actual'
+						value={situacion}
+						onChange={e => setSituacion(e.target.value)}
+						mb='2'
+					>
+						{estilosTarjetas.map(estilo => (
+							<option
+								key={estilo.id}
+								value={estilo.id}
+							>{`${estilo.name}`}</option>
+						))}
+					</Select>
+
+					<Text>Información del perfil</Text>
+					<Select
+						placeholder='Estilo para la tarjeta información del perfil'
+						value={bdeInfo}
+						onChange={e => setBdeInfo(e.target.value)}
+						mb='2'
+					>
+						{estilosTarjetas.map(estilo => (
+							<option
+								key={estilo.id}
+								value={estilo.id}
+							>{`${estilo.name}`}</option>
+						))}
+					</Select>
+
+					<Text>Idiomas y habilidades</Text>
+					<Select
+						placeholder='Estilo para la tarjeta de idiomas y habilidades'
+						value={hab}
+						onChange={e => setHab(e.target.value)}
+						mb='2'
+					>
+						{estilosTarjetas.map(estilo => (
+							<option
+								key={estilo.id}
+								value={estilo.id}
+							>{`${estilo.name}`}</option>
+						))}
+					</Select>
+				</ModalBody>
+				<ModalFooter>
+					<Button
+						colorScheme='blue'
+						onClick={handleGuardar}
+						mr='2'
+					>
+						Guardar
+					</Button>
+					<Button
+						colorScheme='blue'
+						onClick={onClose}
+					>
+						Cerrar
+					</Button>
+				</ModalFooter>
+			</ModalContent>
+		</Modal>
+	)
+}
--- src/constants/DecoratorCard.js
+++ src/constants/DecoratorCard.js
@@ -1,51 +1,49 @@
 export const opcionesColores = [
-    { value: '#FFD700', label: 'Dorado' },
-    { value: '#FF5733', label: 'Rojo' },
-    { value: '#33FF57', label: 'Verde' },
-    { value: '#3375FF', label: 'Azul' },
-    { value: '#8B00FF', label: 'Violeta' }
-  ];
- export const opcionesTipografias = [
-    { value: 'Arial', label: 'Arial' },
-    { value: 'Verdana', label: 'Verdana' },
-    { value: 'Times New Roman', label: 'Times New Roman' },
-    { value: 'Georgia', label: 'Georgia' },
-    { value: 'Courier New', label: 'Courier New' }
-  ];
-    
- export const estilosTarjetas = [
-  {
-    id: 'default',
-    className: 'default',
+	{ value: '#FFD700', label: 'Dorado' },
+	{ value: '#FF5733', label: 'Rojo' },
+	{ value: '#33FF57', label: 'Verde' },
+	{ value: '#3375FF', label: 'Azul' },
+	{ value: '#8B00FF', label: 'Violeta' }
+]
+export const opcionesTipografias = [
+	{ value: 'Arial', label: 'Arial' },
+	{ value: 'Verdana', label: 'Verdana' },
+	{ value: 'Times New Roman', label: 'Times New Roman' },
+	{ value: 'Georgia', label: 'Georgia' },
+	{ value: 'Courier New', label: 'Courier New' }
+]
 
-  },
+export const estilosTarjetas = [
+	{
+		id: 'default',
+		name: 'Tarjeta sin estilo',
+		className: 'default'
+	},
 
-  {
-    id: 'customCardStyle',
-    className: 'estilo_1',
-    contentClassName: 'estilo_card1',
-  
-  },
-  {
-    id: 'customCardStyle2',
-    className: 'estilo_2',
-    contentClassName: 'estilo_card2',
+	{
+		id: 'customCardStyle',
+		name: 'Primer estilo',
+		className: 'estilo_1',
+		contentClassName: 'estilo_card1'
+	},
+	{
+		id: 'customCardStyle2',
+		name: 'Segundo estilo',
+		className: 'estilo_2',
+		contentClassName: 'estilo_card2'
+	},
+	{
+		id: 'customCardStyle3',
+		name: 'Tercer estilo',
+		className: 'estilo_card3',
+		contentClassName: 'estilo_3'
+	},
+	{
+		id: 'customCardStyle4',
+		name: 'Cuarto estilo',
+		className: 'box',
+		contentClassName: 'container'
+	}
 
-  },
-  {
-    id: 'customCardStyle3',
-    className: 'estilo_card3',
-    contentClassName: 'estilo_3',
-  },
-  {
-    id: 'customCardStyle4',
-    className: 'box',
-    contentClassName: 'container',
-
-
-  }
-  
-  
-    // Si quiere puede agregart mas estilos v2
-  ];
-  
+	// Si quiere puede agregart mas estilos v2
+]
